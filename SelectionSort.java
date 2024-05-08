80ffa370f4c520488f4a388d1b20fc2c75553fbb
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-05-08 11:06:05 +0600

    sorting algo

---
+++ SelectionSort.java
@@ -0,0 +1,33 @@
+class SelectionSort {
+    static void Sort(int a[], int n) {
+        int i, j;
+        for (i = 0; i < n - 1; i++) {
+            int iMin = i;
+            for (j = i + 1; j < n; j++) {
+                if (a[j] < a[iMin]) {
+                    iMin = j;
+                }
+            }
+            if (iMin != i) {
+                int temp = a[i];
+                a[i] = a[iMin];
+                a[iMin] = temp;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int a[] = { 1, 3, 5, 7, 9, 0, 8, 6, 4, 2 };
+        int n = a.length;
+        System.out.println("Unsorted Array");
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i]);
+        }
+        Sort(a, n);
+        System.out.println("Sorted Array");
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+}
